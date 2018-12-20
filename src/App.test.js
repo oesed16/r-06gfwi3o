b0adf074b0304cd1f433b6332a913dda792bc1a1
Angela Castagno <oesed16@gmail.com>
2018-12-20 12:35:28 -0500

    Reto Tu primer componente React

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import { App } from './App';
 import { Welcome } from './Welcome';
 import Setup from './Setup';
 
